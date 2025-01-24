37d09e3998c94c519190132e1a0a485d2457c162
Christopher Olivier <cjolivier01@gmail.com>
2025-01-23 16:43:45 -0800

    Fix header

--- src/libtorchaudio/cuctc/src/ctc_prefix_decoder_kernel_v2.cu
+++ src/libtorchaudio/cuctc/src/ctc_prefix_decoder_kernel_v2.cu
@@ -24,6 +24,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <algorithm>
+#include <cfloat>
 #include "ctc_fast_divmod.cuh"
 #include "cub/cub.cuh"
 #include "device_data_wrap.h"
@@ -31,6 +32,7 @@
 #include "include/ctc_prefix_decoder_host.h"
 
 #include "bitonic_topk/warpsort_topk.cuh"
+
 
 namespace cu_ctc {
 __inline__ __device__ float _lauguage() {
