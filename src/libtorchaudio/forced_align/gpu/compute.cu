67e4dc6f4b85aa118a686e639334c6a492d6efed
Chris Olivier <cjolivier01@apache.org>
2025-11-26 18:21:46 -0800

    Fix for cuda 13.0

--- src/libtorchaudio/forced_align/gpu/compute.cu
+++ src/libtorchaudio/forced_align/gpu/compute.cu
@@ -94,7 +94,8 @@ __global__ void falign_cuda_step_kernel(
     alphas_a[curIdxOffset][i] = result + logProbs_a[batchIndex][t][labelIdx];
     threadMax = max(threadMax, alphas_a[curIdxOffset][i]);
   }
-  scalar_t maxResult = BlockReduce(tempStorage).Reduce(threadMax, cub::Max());
+  scalar_t maxResult = BlockReduce(tempStorage).Reduce(threadMax,  cuda::maximum<>{});
+  
   if (threadIdx.x == 0) {
     maxValue = maxResult;
   }
